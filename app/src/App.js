3567b8751861cfcdc524d6177147b8f7d873c9b6
NazarMatkovskyi <matkovskuyn@gmail.com>
2024-03-15 16:16:59 +0200

    test

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aperiam beatae culpa dicta dolorem
+        esse facilis fuga, hic iste magnam maxime non, reprehenderit sed similique tempora, voluptate voluptatem
+        voluptatum.
+      </div>
+    </div>
+  );
+}
+
+export default App;
